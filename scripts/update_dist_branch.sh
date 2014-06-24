a214fce7b258da4f0ff7cdb8d58181ac5cb5655a
Tobias Bosch <tbosch1009@gmail.com>
2014-06-24 13:53:23 -0700

    update_dist_branch: don’t delete non committed directories

--- scripts/update_dist_branch.sh
+++ scripts/update_dist_branch.sh
@@ -5,10 +5,14 @@ shopt -s extglob
 SCRIPT_DIR=$(dirname $0)
 cd $SCRIPT_DIR/..
 
+# keep the .gitignore in the dist branch as well,
+# so we don't have to remove node_modules and bower_components folder
+# while switching...
+cp .gitignore dist
 branch=$(git rev-parse --abbrev-ref HEAD)
 # TODO: Use "dist" branch to serve via https://projects.angularjs.org/
 git checkout gh-pages
-rm -rf !(dist)
+git rm -rf .
 cp -rf dist/* .
 rm -rf dist
 git add . -A
