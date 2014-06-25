b30d3770cdc0ee9927642bc1985fdea2d207b3cd
Tobias Bosch <tbosch1009@gmail.com>
2014-06-25 09:15:11 -0700

    Use Chrome36 for tests on sauce labs

--- protractor-travis.conf.js
+++ protractor-travis.conf.js
@@ -9,7 +9,7 @@ config.multiCapabilities = [{
   'name': 'Angular E2E',
   'tunnel-identifier': process.env.TRAVIS_JOB_NUMBER,
   'build': process.env.TRAVIS_BUILD_NUMBER,
-  'version': '34'
+  'version': '36'
 }/*, {
   'browserName': 'firefox',
   'name': 'Angular E2E',
