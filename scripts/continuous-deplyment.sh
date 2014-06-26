702e3f2b4dc549e75b8cb8f9542e740fbc4d15c7
Tobias Bosch <tbosch1009@gmail.com>
2014-06-25 17:30:16 -0700

    continuous deployment: use dist branch in dist folder

--- scripts/continuous-deplyment.sh
+++ scripts/continuous-deplyment.sh
@@ -4,8 +4,10 @@ set -ex
 SCRIPT_DIR=$(dirname $0)
 cd $SCRIPT_DIR/..
 
+# update ourselves
 git fetch origin master
 git checkout origin/master -f
-npm install
-./node_modules/.bin/bower install
-./node_modules/.bin/gulp build
+
+# clone the dist branch into a folder
+rm -fr dist
+git clone -b dist https://github.com/angular/projects.git
