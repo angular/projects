f09b9a88bb4b57e3dbc6cb1880f929d9af544ccd
marry-poppins <tbosch1009@gmail.com>
2014-06-26 13:31:47 -0700

    chore(deploy): auto deploy, create a real clone of the repo

--- scripts/travis_update_dist_branch.sh
+++ scripts/travis_update_dist_branch.sh
@@ -2,15 +2,23 @@
 set -e
 
 SCRIPT_DIR=$(dirname $0)
+cd $SCRIPT_DIR/..
 
 [ "$TRAVIS_PULL_REQUEST" == "false" ]
 [ "$TRAVIS_BRANCH" == "master" ]
 
 echo -e "Publishing dist branch...\n"
 
-git config --global user.name "marry-poppins"
-$SCRIPT_DIR/update_dist_branch.sh
+git clone https://github.com/angular/projects.git -b dist temp
+cd temp
 
-git push -fq https://${GH_TOKEN}@github.com/angular/projects.git dist > /dev/null
+git config --global user.name "marry-poppins"
+
+rm -rf .
+cp -rf ../dist/ .
+git add . -A
+git commit -m "update site from src" || true
+
+git push -fq https://${GH_TOKEN}@github.com/angular/projects.git dist >/dev/null 2>&1
 
 echo -e "Published build to dist branch.\n"
