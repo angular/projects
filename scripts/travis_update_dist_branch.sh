90f4cd5d4d8ec4abecc8b2359149fb9985b0a902
Tobias Bosch <tbosch1009@gmail.com>
2014-06-26 13:57:07 -0700

    chore(build): update auto deploy script

--- scripts/travis_update_dist_branch.sh
+++ scripts/travis_update_dist_branch.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-set -e
+set -ex
 
 SCRIPT_DIR=$(dirname $0)
 cd $SCRIPT_DIR/..
@@ -9,16 +9,21 @@ cd $SCRIPT_DIR/..
 
 echo -e "Publishing dist branch...\n"
 
+rm -fr temp
 git clone https://github.com/angular/projects.git -b dist temp
 cd temp
 
-git config --global user.name "marry-poppins"
+git config user.name "Marry Poppins"
+git config user.email "marry-poppins@angularjs.org"
 
-rm -rf .
+rm -rf *
 cp -rf ../dist/ .
 git add . -A
 git commit -m "update site from src" || true
 
+echo "Pushing..."
+# Make sure we don't log the GH_TOKEN in any case!
+set +x
 git push -fq https://${GH_TOKEN}@github.com/angular/projects.git dist >/dev/null 2>&1
 
 echo -e "Published build to dist branch.\n"
